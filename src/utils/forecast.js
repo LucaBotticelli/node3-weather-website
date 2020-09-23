6994b13699cc63e644d29daf0ba31b6a2d5a2e82
Luca Botticelli <luca.botticelli216@gmail.com>
2020-09-23 12:45:19 +0200

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,15 +10,16 @@ const forecast = (latitude, longitude, callback) => {
 
             if (error) {
                 callback('Unable to connect to weather service!', undefined )
-            } else if (body.error ) {
+            } else if (body.error) {
                 callback('Unable to find location. Try another search', undefined )
             } else {
-                callback(undefined, "It's " + body.current.weather_descriptions + ". There are " + body.current.temperature + " degrees with a " + body.current.precip + "% chance of rain."
+                callback(undefined, "It's " + body.current.weather_descriptions + ". There are " + body.current.temperature + " degrees (" +body.current.humidity + "% Humidity) with a " + body.current.precip + "% chance of rain."
                 )
             }
 
         }
     )
+   
 
 }
 module.exports = forecast;
